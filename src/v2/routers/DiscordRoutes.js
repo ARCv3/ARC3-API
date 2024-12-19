e65b8ffb7becb63f0a0ea98961f2e72617e572c4
izzydotexe <izzydotexe@github.io>
2024-12-18 22:19:24 -0500

    remove code dupe

--- src/v2/routers/DiscordRoutes.js
+++ src/v2/routers/DiscordRoutes.js
@@ -3,11 +3,8 @@ const router = express.Router();
 
 const authenticated = require('../../auth/middlewares/authenticated.js')
 
-const { GetUser, GetGuild, GetMe, GetGuilds } = require('../controllers/DiscordControllers.js');
+const { GetMe } = require('../controllers/DiscordControllers.js');
 
-router.get('/users/:id', authenticated, GetUser);
-router.get('/guilds/:id', authenticated, GetGuild);
 router.get('/me', authenticated, GetMe);
-router.get('/me/guilds', authenticated, GetGuilds);
 
 module.exports = router;
